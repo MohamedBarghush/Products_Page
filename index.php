8e4679583867bdd10ec32ee9cf8f98625b59d5fa
Mohamed ElShab <cinosmoh@gmail.com>
2022-05-04 21:55:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   include "Includes/dbh.ink.php";
   include "Includes/user.ink.php";
   include "Includes/viewUser.ink.php";
+  include "Includes/deleteItem.ink.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,20 +11,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Learning</title>
+    <title>Products Page</title>
 </head>
 <body>
-    <button type="button" onclick="EnableEditForm()" id="AddButton" class="btn">ADD</button>
-    <button type="button" onclick="MassDelete()" id="delete-product-btn" class="btn">MASS DELETE</button><br>
+    <?php
+        $deleter = new DeleteItem();
+        if (isset($_POST['mass-delete'])) {
+            $arr = explode("-", $_POST['mass-delete']);
+            foreach ($arr as $item) {
+                $deleter->delete($item, "products");
+            }
+        }
+    ?>
+    <button type="button" onclick="window.location.replace('add-product.php');" id="AddButton" class="btn">ADD</button>
+    <form method="POST" style="display:inline-block;"><input type="submit" id="delete-product-btn" class="btn" name="mass-delete" value="MASS DELETE"></form><br>
     <hr class="solid">
     <div class="grid-container">
         <?php
-          $users = new ViewUser();
-          $users->ShowAllUsers();
+          $users = new ViewProduct();
+          $users->ShowAllProducts();
         ?>
     </div>
     <hr class="solid">
     <div class="footer"><label>Scandiweb Test assignment</label></div>
-    <script src="app.js"></script>
+    <script src="app.js">
+      
+    </script>
 </body>
 </html>
