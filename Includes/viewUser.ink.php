75775127b64f6ae29540481336a7cd172024562a
Mohamed ElShab <cinosmoh@gmail.com>
2022-05-04 21:59:43 +0200

    Update viewUser.ink.php

--- Includes/viewUser.ink.php
+++ Includes/viewUser.ink.php
@@ -1,22 +1,24 @@
 <?php
 
-class ViewUser extends User{
-    public function ShowAllUsers () {
-        $datas = $this->GetAllUsers();
-        foreach ($datas as $data) {
-            echo "<div class='grid-item' db_id='".$data["id"]."'>
-                    <input type='checkbox' class='delete-checkbox'>";
-            echo $data["SKU"]."<br>";
-            echo $data["NAME"]."<br>";
-            echo $data["PRICE"].".00$<br>";
-            if ($data["TYPE"] == "DVD") {
-                echo "Size: ".$data["SIZE"]." MB<br>";
-            } else if ($data["TYPE"] == "Book") {
-                echo "Weight: ".$data["WEIGHT"]." KG<br>";
-            } else if ($data["TYPE"] == "Furniture") {
-                echo "Dimension: ".$data["HEIGHT"]."x".$data["WIDTH"]."x".$data["LENGTH"]."<br>";
+class ViewProduct extends Product{
+    public function ShowAllProducts () {
+        if ($this->GetAllProducts() != null) {
+            $datas = $this->GetAllProducts();
+            foreach ($datas as $data) {
+                echo "<div class='grid-item' db_id='".$data["id"]."'>
+                        <input type='checkbox' class='delete-checkbox'>";
+                echo $data["SKU"]."<br>";
+                echo $data["NAME"]."<br>";
+                echo $data["PRICE"].".00$<br>";
+                if ($data["TYPE"] == "DVD") {
+                    echo "Size: ".$data["ATTRIBUTE"]."<br>";
+                } else if ($data["TYPE"] == "Book") {
+                    echo "Weight: ".$data["ATTRIBUTE"]."<br>";
+                } else if ($data["TYPE"] == "Furniture") {
+                    echo "Dimensions: ".$data["ATTRIBUTE"]."<br>";
+                }
+                echo "</div>";
             }
-            echo "</div>";
         }
     }
 }
